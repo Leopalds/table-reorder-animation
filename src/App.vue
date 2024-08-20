c3aa20a09c27b321816447d167208c0d785781ee
Pedro Leopoldo <79977884+Leopalds@users.noreply.github.com>
2024-08-20 15:23:26 -0300

    Adding table component

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,62 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <table>
+    <thead>
+      <tr class="header">
+        <th class="header-row" v-for="header in data.headers" :key="header">{{ header }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="body-row" v-for="row in Object.values(data.rows)" :key="row.id">
+        <td class="body-cell" v-for="cell in Object.values(row)" :key="cell">{{ cell }}</td>
+      </tr>
+    </tbody>
+  </table>
+  
+  <div class="reorder-button" @click="reorder">Reorder</div>
 </template>
 
+<script setup>
+import { ref } from 'vue'
+
+const data = ref({
+  headers: ['id', 'name', 'amount', 'price'],
+  rows: [
+      { id: 1, name: 'Apple', amount: 10, price: 1.5 },
+      { id: 2, name: 'Banana', amount: 20, price: 2.5 },
+      { id: 3, name: 'Cherry', amount: 30, price: 3.5 },
+      { id: 4, name: 'Date', amount: 40, price: 4.5 },
+      { id: 5, name: 'Elderberry', amount: 50, price: 5.5 },
+      { id: 6, name: 'Fig', amount: 60, price: 6.5 },
+      { id: 7, name: 'Grape', amount: 70, price: 7.5 },
+      { id: 8, name: 'Honeydew', amount: 80, price: 8.5 },
+      { id: 9, name: 'Kiwi', amount: 90, price: 9.5 },
+      { id: 10, name: 'Lemon', amount: 100, price: 10.5 }
+  ]
+})
+
+const reorder = () => {
+  data.value.rows = data.value.rows.sort(() => Math.random() - 0.5)
+}
+
+
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.reorder-button{
+  cursor: pointer;
+  padding: 10px;
+  background-color: #007bff;
+  color: white;
+  text-align: center;
+  margin-top: 10px;
+  margin-left: 10px;
+  height: fit-content;
+  border-radius: 12px;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+table, th, td{
+    border-collapse: collapse; /*define a separação entre as bordar*/
+    padding: 10px;
+    text-align: left;
+    border: 1px solid rgb(160 160 160) !important;
 }
 </style>
